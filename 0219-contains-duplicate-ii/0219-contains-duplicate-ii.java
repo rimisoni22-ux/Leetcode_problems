c02867e709ce57f6d0aa9e9731d426b427f1a185
Rimi <rimisoni22@gmail.com>
2026-06-27 15:38:51 +0530

    Time: 1168 ms (6.48%), Space: 69.4 MB (99.25%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n && j <= i + k; j++) { // i-j <=k
+                if (nums[i] == nums[j]) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
